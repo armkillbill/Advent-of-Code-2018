20c00a467d36b2abfd72305fc7e650341cb7acc8
armkillbill <arm_killbill@hotmail.com>
2018-12-04 06:16:40 +0700

    Add completed code for Day-1

--- Day-1/day1.go
+++ Day-1/day1.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var sum int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		str := scanner.Text()
+		num, _ := strconv.Atoi(str)
+		sum += num
+	}
+
+	fmt.Println("sum = ", sum)
 }
