769869d91e9ebecae6a1d7bd85f8324ac86d8d21
armkillbill <arm_killbill@hotmail.com>
2018-12-05 16:46:30 +0700

    Add comments to day5p1.go

--- Day-5/Part-1/day5p1.go
+++ Day-5/Part-1/day5p1.go
@@ -23,6 +23,7 @@ func main() {
 	for hasDelete {
 		hasDelete = false
 		for i := 0; i < len(input)-1; {
+			// I try to use math.Abs here but not success
 			if input[i]-input[i+1] == 32 || input[i+1]-input[i] == 32 {
 				input = input[:i] + input[i+2:]
 				hasDelete = true
